092d854e7feefc879d2c1190bd6e5a612ef9e3c2
Klaas van der Werk <klaas.vanderwerk@gmail.com>
2020-10-01 13:22:21 +0200

    add tailwind css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './app.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+import './assets/styles/index.css'
+
 Vue.config.productionTip = false
 
 new Vue({
