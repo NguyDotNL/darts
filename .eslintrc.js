39336b6f933542cd6367f563abe3b1304b78a636
Klaas van der Werk <klaas.vanderwerk@gmail.com>
2020-10-01 12:29:45 +0200

    Initial project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     'eslint:recommended'
   ],
   parserOptions: {
@@ -12,6 +12,28 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-var": "error",
+    "indent": ["error", 2],
+    "quotes": ["warn", "single"],
+    "semi": ["warn", "never"],
+    "comma-dangle": ["warn", "always-multiline"],
+    "no-async-promise-executor": 0,
+    "no-case-declarations": 0,
+    "no-control-regex": 0,
+    "no-new-func": 0,
+    "no-prototype-builtins": 0,
+    "no-useless-call": 0,
+    "no-useless-catch": 0,
+    "no-useless-rename": 0,
+    "vue/max-attributes-per-line": ["warn", {
+        "singleline": 3,
+        "multiline": {
+          "max": 1,
+          "allowFirstLine": false
+        }
+    }],
+    "vue/require-default-prop": 0,
+    "vue/singleline-html-element-content-newline": 0
   }
 }
