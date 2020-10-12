2163ae4de6af3775e989c8739a3837ffefa50dfc
Harmbogert <harm.vandenbogert@outlook.com>
2020-10-12 16:33:44 +0200

    match-table footer
    database connectie changes

--- src/clients/dashboard.client.js
+++ src/clients/dashboard.client.js
@@ -3,16 +3,19 @@ import { matches } from '@/plugins/firebase'
 const DashboardClient = {
   getAllMatches: (options) => options.iKnowThisCanBeALotOfDataThatIProbablyDontNeed === true &&
     matches.once('value').then(snapshot => snapshot.val()),
-  getMatchesPerPage: (offset, date = null) => {
-    const query = date == null
+
+  getMatchesPerPage: (offset, date = null, type = false) => {
+    const query = !type
       ? matches.orderByChild('date').limitToLast(offset)
-      : matches.orderByChild('date').endAt(date).limitToLast(offset)
+      : type == 'prev' ? matches.orderByChild('date').limitToFirst(offset).startAt(date)
+        : type == 'next' ? matches.orderByChild('date').limitToLast(offset).endAt(date) : ''
+
     return query.once('value').then(snapshot => Object.values(snapshot.val()).sort((a, b) => b.date - a.date))
   },
-  searchMatchesByName: (queryText) => matches.orderByChild('matchName').startAt(queryText).endAt(queryText+'\uf8ff').limitToFirst(10)
-    .once('value').then(snapshot => Object.values(snapshot.val()).sort((a,b) => 
+  searchMatchesByName: (queryText, itemsPerPage) => matches.orderByChild('matchName').startAt(queryText).endAt(queryText+'\uf8ff').limitToFirst(itemsPerPage)
+    .once('value').then(snapshot => snapshot.val() ? Object.values(snapshot.val()).sort((a,b) =>
       a.matchName < b.matchName ? -1 : a.matchName > b.matchName ? 1 : 0,
-    )),
+    ) : []),
 }
 
 export default DashboardClient
