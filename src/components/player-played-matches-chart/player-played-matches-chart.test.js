b94f50f2d9261129394efba2e8698253a127fa0a
NguyNL <nguydotnl@gmail.com>
2020-10-12 21:21:01 +0200

    Pull request created .mock.js file

--- src/components/player-played-matches-chart/player-played-matches-chart.test.js
+++ src/components/player-played-matches-chart/player-played-matches-chart.test.js
@@ -1,27 +1,12 @@
 import { render, Vue } from '@/test/test-wrapper'
 import PlayerPlayedMatchesChart from './player-played-matches-chart.vue'
+import MockData from './player-played-matches-chart.mock'
 
 test('Expect to show played matches 1', async () => {
   const { getByText } = render(PlayerPlayedMatchesChart, { 
     props: {
-      player: {
-        playerId: 'dit-is-een-id',
-        firstName: 'Jantje',
-        lastName: 'Bakker',
-      },
-      matches: [
-        {
-          bestOfLegs: 1,
-          bestofSets: 5,
-          date: 1594408074,
-          matchId: '10b8d8df-ff96-4f09-9338-c9ecce6e1719',
-          matchName: 'Test WK 94',
-          players: Object,
-          startPoints: 501,
-          winner: 'd88033b9-14fe-41f6-98d2-f9f35d7024a1',
-          winnerName: 'Francis Hoenselaar',
-        },
-      ],
+      player: MockData.player,
+      matches: MockData.matches,
     },
   })
 
