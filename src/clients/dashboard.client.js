fc11dffd62ef84273e58dbe0457f5f18a66caabf
Klaas van der Werk <klaas.vanderwerk@gmail.com>
2020-10-12 23:33:48 +0200

    export matches function

--- src/clients/dashboard.client.js
+++ src/clients/dashboard.client.js
@@ -1,4 +1,5 @@
-import { matches } from '@/plugins/firebase'
+import { matches, matchDetails } from '@/plugins/firebase'
+import { saveAs } from 'file-saver'
 
 const DashboardClient = {
   getAllMatches: (options) => options.iKnowThisCanBeALotOfDataThatIProbablyDontNeed === true &&
@@ -13,6 +14,17 @@ const DashboardClient = {
     .once('value').then(snapshot => Object.values(snapshot.val()).sort((a,b) => 
       a.matchName < b.matchName ? -1 : a.matchName > b.matchName ? 1 : 0,
     )),
+  exportMatches: async (matchIds) => {
+    const matchesData = matchIds.map(async matchId => {
+      const match = await matches.child(matchId).once('value').then(snapshot => snapshot.val())
+      const matchDetailsData = await matchDetails.child(matchId).once('value').then(snapshot => snapshot.val())
+      return { match, matchDetails: matchDetailsData }
+    })
+    Promise.all(matchesData).then((values) => {
+      let blob = new Blob([JSON.stringify({ matches: values }, null, 2)], { type: 'text/plain;charset=utf-8' })
+      saveAs(blob, 'dartinmolema.json')
+    })
+  },
 }
 
 export default DashboardClient
