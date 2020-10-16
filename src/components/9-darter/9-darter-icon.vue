440c2b1b695628c5cbeccef24defaaebd1078c65
NguyNL <nguydotnl@gmail.com>
2020-10-16 17:40:35 +0200

    Create match bugfix, changed 9-darter icon

--- src/components/9-darter/9-darter-icon.vue
+++ src/components/9-darter/9-darter-icon.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="bg-yellow-700 text-gray-800 rounded-circle text-sm pr-1 pl-1">9</span>
+  <span class="bg-yellow-400 text-gray-800 rounded-full text-sm" style="display: inline-block; width: 21px; height: 21px; text-align: center; vertical-align: baseline; line-height:21px;">9</span>
 </template>
 
 <script>
