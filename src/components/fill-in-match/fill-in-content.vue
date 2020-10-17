20714a6fe3dc07dacebb45e785a150f73616ecc2
Jan Kuipers <Orriginal@live.nl>
2020-10-17 15:40:40 +0200

    Turn based system for adding throws

--- src/components/fill-in-match/fill-in-content.vue
+++ src/components/fill-in-match/fill-in-content.vue
@@ -6,19 +6,23 @@
         <FillInTable
           :title="players[0].playerName"
           :player-key="players[0].playerKey"
-          :turn-data="legData.players[players[0].playerKey]"
+          :turn-data="getTurnData(players[0].playerKey)"
           :winner="legData.winner === players[0].playerKey"
           :start-points="matchData.match.startPoints"
           :is-last-set-and-leg="isLastSetAndLeg"
+          :leg-winner="!!legData.winner"
+          :player-has-turn="turn === 1 || !turn"
           @update="updateThrow"
         />
         <FillInTable
           :title="players[1].playerName"
           :player-key="players[1].playerKey"
-          :turn-data="legData.players[players[1].playerKey]"
+          :turn-data="getTurnData(players[1].playerKey)"
           :winner="legData.winner === players[1].playerKey"
           :start-points="matchData.match.startPoints"
           :is-last-set-and-leg="isLastSetAndLeg"
+          :leg-winner="!!legData.winner"
+          :player-has-turn="turn === 2 || !turn"
           @update="updateThrow"
         />
       </v-row>
@@ -78,6 +82,7 @@ export default {
       onTurn: 1,
       legData: false,
       legWinner: false,
+      turn: false,
     }
   },
   watch: {
@@ -124,9 +129,57 @@ export default {
       this.legData = legs[legsKeys[this.leg-1]]
       const legWinner = playerKeys.indexOf(this.legData.winner)
       this.legWinner = legWinner > -1 ? legWinner : false
+
+      this.updatePlayerTurn()
+    },
+    updatePlayerTurn() {
+      const player1Key = this.players[0].playerKey
+      const player2Key = this.players[1].playerKey
+      const playersData = this.legData.players
+      const player1Turns = playersData[player1Key]
+      const player2Turns = playersData[player2Key]
+
+      if(player1Turns === '' && player2Turns === '') return
+
+      const lastTurnPlayer1 = player1Turns !== '' ? player1Turns[player1Turns.length - 1] : false
+      const lastTurnPlayer2 = player2Turns !== '' ? player2Turns[player2Turns.length - 1] : false
+
+      if(!lastTurnPlayer1 && lastTurnPlayer2) {
+        this.turn = this.playersTurnIsNotActive(lastTurnPlayer2) ? 1 : 2
+      } else if(lastTurnPlayer1 && !lastTurnPlayer2) {
+        this.turn = this.playersTurnIsNotActive(lastTurnPlayer1) ? 2 : 1
+      } else if(player1Turns.length > player2Turns.length) {
+        this.turn = this.playersTurnIsNotActive(lastTurnPlayer1) ? 2 : 1
+      } else if(player1Turns.length < player2Turns.length) {
+        this.turn = this.playersTurnIsNotActive(lastTurnPlayer2) ? 1 : 2
+      } else if(player1Turns.length === player2Turns.length) {
+        const player1LastTurnNotActive = this.playersTurnIsNotActive(lastTurnPlayer1)
+        const player2LastTurnNotActive = this.playersTurnIsNotActive(lastTurnPlayer2)
+        
+        if(player1LastTurnNotActive && player2LastTurnNotActive) {
+          this.turn = this.turn === 1 ? 2 : this.turn === 2 ? 1 : false
+        } else if (!player1LastTurnNotActive) {
+          this.turn = 1
+        } else if(!player2LastTurnNotActive) {
+          this.turn = 2
+        }
+      } else {
+        this.turn = false
+      }
+    },
+    playersTurnIsNotActive(lastTurn) {
+      const lastThrow = lastTurn.throws[lastTurn.throws.length - 1]
+      const throwsIs3AndNoWinner = lastTurn.throws.length === 3 && !this.legData.winner
+      const lastTurnBust = !this.legData.winner && lastTurn.total === 0 && lastThrow.multiplier !== 0 && lastTurn.throws.length < 3
+
+      return throwsIs3AndNoWinner || lastTurnBust
     },
     updateThrow(data) {
       this.$emit('update', data)
+    },
+    getTurnData(playerKey) {
+      const turnData = this.legData.players[playerKey]
+      return turnData !== '' ? turnData : []
     },
   },
 }
--- src/components/fill-in-match/fill-in-table.vue
+++ src/components/fill-in-match/fill-in-table.vue
@@ -11,8 +11,10 @@
       :key="key"
       :turn-data="turn"
       :turn="key+1"
-      :is-last="key === (turns.length-1) && isLastSetAndLeg"
+      :is-last="(key === (turns.length-1) || key === (turns.length-1-lastTurnToNotCount)) && isLastSetAndLeg"
       :winner="winner"
+      :player-has-turn="playerHasTurn"
+      :leg-winner="legWinner"
       @update="updateThrow"
     />
   </v-col>
@@ -48,11 +50,20 @@ export default {
       type: Boolean,
       required: true,
     },
+    legWinner: {
+      type: Boolean,
+      required: false,
+    },
+    playerHasTurn: {
+      type: Boolean,
+      required: false,
+    },
   },
   data: function () {
     return {
       turnPoints: [],
       turns: [],
+      lastTurnToNotCount: 0,
     }
   },
   watch: {
@@ -60,9 +71,21 @@ export default {
       immediate: true,
       handler: 'calculateTurnPoints',
     },
+    'playerHasTurn': {
+      immediate: true,
+      handler: 'calculateTurnPoints',
+    },
   },
   methods: {
     calculateTurnPoints() {
+      this.turns = this.playerHasTurn ? [{
+        remainingPoints: this.startPoints,
+        throws: {},
+        total: 0,
+      }] : []
+
+      if(!this.turnData.length) return
+      
       let remainingPoints = this.startPoints
       let lastTotal = 0
       this.turns = this.turnData.map(turn => {
@@ -73,8 +96,30 @@ export default {
           remainingPoints,
         }
       })
+      
+      const lastTurn = this.turns[this.turns.length - 1]
+      const lastThrow = lastTurn.throws[lastTurn.throws.length - 1]
+      remainingPoints -= lastTurn.total
+
+      const throwsIs3AndNoWinner = lastTurn.throws.length === 3 && !this.legWinner
+      const lastTurnBust = !this.legWinner && lastTurn.total === 0 && lastThrow.multiplier !== 0 && lastTurn.throws.length < 3
+
+      if((throwsIs3AndNoWinner || lastTurnBust) && remainingPoints > 1 && this.playerHasTurn) {
+        this.lastTurnToNotCount = 1
+        this.turns.push({
+          remainingPoints: remainingPoints,
+          throws: {},
+          total: 0,
+        })
+      }
+      
     },
     updateThrow(data) {
+      if(this.lastTurnToNotCount) {
+        if(data.turn === this.turns.length - 1) {
+          this.lastTurnToNotCount = 0
+        }
+      }
       this.$emit('update', { ...data, playerKey: this.playerKey })
     },
   },
